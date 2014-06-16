ad2d98b5a90efcca18d9ba580d2e005690f3a880
Gabe Polk <gabe.a.polk@gmail.com>
2014-06-15 19:06:49 -0700

    Finished translation of words that begin with vowels, consonants

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -1,4 +1,24 @@
 require "pig_latin/version"
 
 module PigLatin
+
+  def self.translate(str)
+
+    # starts with y
+    if str[/\Ay/] != nil
+      str[0] = ""
+      return str + "yay"
+    # no vowels, has y
+    elsif str[/[aeiou]/] == nil && str[/y/] != nil
+      str[/y.*/] + str[/^[^y]*/] + "ay"
+    # begins with a vowel
+    elsif str[/^[aeiou]/] != nil
+      str + "way"
+    # begins with a consonant
+    elsif str[/^[b-df-hj-np-tv-z]*/] != nil
+      first_chars = str[/^[b-df-hj-np-tv-z]*/]
+      num_chars = first_chars.length
+      str[num_chars..-1] + first_chars + "ay"
+    end
+  end
 end
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -2,37 +2,49 @@ require 'spec_helper'
 
 describe PigLatin do
   it "is a Module" do
+
     expect(PigLatin).to be_a(Module)
   end
 end
 
 describe ".translate" do
-  it "takes a str as an argument" do
-    glove = PigLatin.translate("glove")
+  # it "takes a str as an argument" do
+  #   glove = PigLatin.translate("glove")
 
-    expect(glove).to eq("glove")
-  end
 
+  #   expect(glove).to eq("glove")
+  # end
 
   it "correctly translates words that begin with consonants" do
     battle = PigLatin.translate("battle")
     glove = PigLatin.translate("glove")
+
+    # binding.pry
 
     expect(battle).to eq("attlebay")
     expect(glove).to eq("oveglay")
   end
 
   it "correctly translates words that begin with vowels" do
-    egg = PigLatin.translate("eggway")
+    egg = PigLatin.translate("apple")
+    # binding.pry
 
     expect(egg).to eq("appleway")
   end
 
-  it "correctly translates words that begin with y/contain y" do
+  it "correctly translates words that begin with y" do
+    # binding.pry
     yellow = PigLatin.translate("yellow")
-    rhythm = PigLatin.translate("rhythm")
+
 
     expect(yellow).to eq("ellowyay")
+  end
+
+  it "correctly translates words that contain only y" do
+    rhythm = PigLatin.translate("rhythm")
+    my = PigLatin.translate("my")
+
     expect(rhythm).to eq("ythmrhay")
+    expect(my).to eq("ymay")
   end
 end
